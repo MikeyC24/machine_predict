5c2d7229bebf19742902bc14b0328d23c027db69
mikeyc24 <mgchapman24@gmail.com>
2017-09-05 17:54:24 -0400

    check ruby project on how to structure input menu

--- user_guide_input.py
+++ user_guide_input.py
@@ -18,53 +18,57 @@ data_instace = ArrangeDataInOrder(df)
 # options could be see data, add columns (moving avg), remove (columns, missing rows)
 # graphs/charts/story, calculate (avg, std) then rdy for models 
 user_interface_input_options_show = ['add/change', 'remove', 'show', 'start_model']
-print('welcome. Now that your data is loaded would type "add/change" or 1 to \
+user_interface_input_options = {'add/change':0, 'remove':1, 'show':2, 'start_model':3}
+time_series_input = []
+#. 1. is the data a time series or not
+time_series_question0 = ' Is this data a time series? This means, order and time matter \
+and you will be predicting a next value some time out. type "yes" if that is the case\
+otherwise enter "no": '
+time_series_input.append(str(input(time_series_question0)))
+print(time_series_input[0])
+if time_series_input[0] == 'yes':
+	time_series_question1 = 'Is this data missing any time gaps? If it is, enter a \
+	start date, end date and time freq, index/date column in this format "start_date, \
+	end_date, frequency, index, estimate". the correct date formate is 2017-08-07 12:40:00. \
+	The correct time interval is 10Min or 1H. enter yes for estimate, if you \
+	you would like missing values estimated. If is not missing data enter "no": '
+	time_series_input.append(input(str(time_series_question1)).split(','))
+	time_series_question2 = 'Would you like to group this data by a different time\
+	interval? If no enter "no". If yes enter time interval and whether you would \
+	like blank values to be estimated in yes or no fashion. enter format like\
+	time_interval, estimate.: '
+	time_series_input.append(input(str(time_series_question2)).split(','))
+	for answer in time_series_input:
+		print(type(answer))
+		print(answer)
+	try:
+		if time_series_input[1][0] != 'no' or time_series_input[2][0] != 'no':
+			print('preparing data')
+			df = data_instace.fill_in_data_full_range(time_series_input[1][0], 
+									time_series_input[1][1], time_series_input[1][2],
+									index=time_series_input[1][3], interpolate=time_series_input[1][4])
+			data_instace2  = ArrangeDataInOrder(df)
+			df_new = data_instace2.group_by_time_with_vars(time_series_input[2][0], 
+					reset_index='no', interpolate=time_series_input[2][1], 
+					index='no', set_to_datetime='no')
+		print(df_new.head(10))
+	except:
+		print('data does not neet time preparing or time preparing went wrong')
+print('Now that your data is loaded and we have determined if it is a time series or not \
+ . we now need to decide if that data needs to be arranged. type "add/change" or 1 to \
 	manipulate rows/columns, type remove or 2 to drop columns/rows, type show or 3\
 	to view certain parts of your data, and lastly type start model or 4 to run models')
-user_interface_input = input('what would you like to do: ')
-while user_interface_input != 'start model' or str(4):
-	print(type(user_interface_input))
-	print(user_interface_input)
-	time_series_input = []
-	#. 1. is the data a time series or not
-	time_series_question0 = ' Is this data a time series? This means, order and time matter \
-	and you will be predicting a next value some time out. type "yes" if that is the case\
-	otherwise enter "no": '
-	time_series_input.append(str(input(time_series_question0)))
-	print(time_series_input[0])
-	if time_series_input[0] == 'yes':
-		time_series_question1 = 'Is this data missing any time gaps? If it is, enter a \
-		start date, end date and time freq, index/date column in this format "start_date, \
-		end_date, frequency, index, estimate". the correct date formate is 2017-08-07 12:40:00. \
-		The correct time interval is 10Min or 1H. enter yes for estimate, if you \
-		you would like missing values estimated. If is not missing data enter "no": '
-		time_series_input.append(input(str(time_series_question1)).split(','))
-		time_series_question2 = 'Would you like to group this data by a different time\
-		interval? If no enter "no". If yes enter time interval and whether you would \
-		like blank values to be estimated in yes or no fashion. enter format like\
-		time_interval, estimate.: '
-		time_series_input.append(input(str(time_series_question2)).split(','))
-		for answer in time_series_input:
-			print(type(answer))
-			print(answer)
-		try:
-			if time_series_input[1][0] != 'no' or time_series_input[2][0] != 'no':
-				print('preparing data')
-				df = data_instace.fill_in_data_full_range(time_series_input[1][0], 
-										time_series_input[1][1], time_series_input[1][2],
-										index=time_series_input[1][3], interpolate=time_series_input[1][4])
-				data_instace2  = ArrangeDataInOrder(df)
-				df_new = data_instace2.group_by_time_with_vars(time_series_input[2][0], 
-						reset_index='no', interpolate=time_series_input[2][1], 
-						index='no', set_to_datetime='no')
-			print(df_new.head(10))
-		except:
-			print('data does not neet time preparing or time preparing went wrong')
+user_interface_input = input('but first what type of model would you like to run: ')
+# maybe add an explanation here or linear and classification
+while user_interface_input != 'start model':
 	prediction_type = input('Will your predication be classification or linear?: ')
 	if prediction_type == 'linear':
 		print('this model is not yet supported')
 	elif prediction_type == 'classification':
-		print('please answer the follwoing qestions to best prepare the data')
+		print('Lets get to work on the data, would you like to add/change, remove\
+			or show')
+		user_interface_input = input('please select: ')
+		
 
 
 
