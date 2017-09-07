e5d6f4a3d6e0b180b67a06a6ebcc67d34441c4f1
Mike <mgchapman24@gmail.com>
2017-09-07 17:13:04 -0400

    user menu, getting multiple vars from user to work in methods

--- user_guide_input.py
+++ user_guide_input.py
@@ -7,19 +7,19 @@ class UserMenu:
 
 	# method descriptions
 	# add/change
-	map_target_for_binary_info = 'This help creates a yes/no or in machine learning a 1,0 for \
-	the target variable. To change, enter the target column name, the yes/1 variable in the column\
-	and the no/1 variable in the order target,yes,no. This is case sensitive'
+	map_target_for_binary_info = """This help creates a yes/no or in machine learning a 1,0 for\
+the target variable. To change, enter the target column name, the yes/1 variable in the column\
+and the no/1 variable in the order target,yes,no. This is case sensitive"""
 	#remove
-	drop_certain_percent_missing_data_info = 'This method will drop all columns that is missing desired up to or greater\
-	than the entered percent. Please provde a percent in decimal form '
-	drop_columns_array_info = 'this method will drop the names of the columns entered\
-	Please enter in the format col_1, col_2. This is case sensitive'
+	drop_certain_percent_missing_data_info = """This method will drop all columns that is missing desired up to or greater\
+than the entered percent. Please provde a percent in decimal form"""
+	drop_columns_array_info = """this method will drop the names of the columns entered\
+Please enter in the format col_1, col_2. This is case sensitive"""
 	#show
-	show_data_frame_info = 'this will provide a snapshot of the dataframe, including the columns,\
-	values in columns to desired number and the shape/dimensions. Please enter a number'
-	show_unique_count_each_col_info = 'This method will show the column name and how many unique \
-	values are in each column'
+	show_data_frame_info = """this will provide a snapshot of the dataframe, including the columns,\
+values in columns to desired number and the shape/dimensions. Please enter a number"""
+	show_unique_count_each_col_info = """This method will show the column name and how many unique\ 
+values are in each column"""
 
 	master_menu_actions = {'add/change': {1:['map_target_for_binary', map_target_for_binary_info] },
 						'remove':{1:['drop_certain_percent_missing_data', drop_certain_percent_missing_data_info],
@@ -50,23 +50,23 @@ class UserMenu:
 	user_interface_input_options = {'add/change':0, 'remove':1, 'show':2, 'start_model':3}
 	time_series_input = []
 	#. 1. is the data a time series or not
-	time_series_question0 = 'Before we get started, we need to know.....\
-	Is this data a time series? This means, order and time matter \
-	and you will be predicting a next value some time out. type "yes" if that is the case\
-	otherwise enter "no": '
+	time_series_question0 = """Before we get started, we need to know.....\
+Is this data a time series? This means, order and time matter\
+and you will be predicting a next value some time out.\
+type "yes" if that is the case otherwise enter "no": """
 	time_series_input.append(str(input(time_series_question0)))
 	print(time_series_input[0])
 	if time_series_input[0] == 'yes':
-		time_series_question1 = 'Is this data missing any time gaps? If it is, enter a \
-		start date, end date and time freq, index/date column in this format "start_date, \
-		end_date, frequency, index, estimate". the correct date formate is 2017-08-07 12:40:00. \
-		The correct time interval is 10Min or 1H. enter yes for estimate, if you \
-		you would like missing values estimated. If is not missing data enter "no": '
+		time_series_question1 = """Is this data missing any time gaps? If it is, enter a 
+		start date, end date and time freq, index/date column in this format "start_date, 
+		end_date, frequency, index, estimate". the correct date formate is 2017-08-07 12:40:00. 
+		The correct time interval is 10Min or 1H. enter yes for estimate, if you 
+		you would like missing values estimated. If is not missing data enter "no": """
 		time_series_input.append(input(str(time_series_question1)).split(','))
-		time_series_question2 = 'Would you like to group this data by a different time\
-		interval? If no enter "no". If yes enter time interval and whether you would \
-		like blank values to be estimated in yes or no fashion. enter format like\
-		time_interval, estimate.: '
+		time_series_question2 = """Would you like to group this data by a different time
+		interval? If no enter "no". If yes enter time interval and whether you would 
+		like blank values to be estimated in yes or no fashion. enter format like
+		time_interval, estimate.: """
 		time_series_input.append(input(str(time_series_question2)).split(','))
 		for answer in time_series_input:
 			print(type(answer))
@@ -89,35 +89,48 @@ class UserMenu:
 		data_instace = ArrangeDataInOrder(df_time)
 	else:
 		data_instace = ArrangeDataInOrder(df)
-	print('Now that your data is loaded and we have determined if it is a time series or not \
-	 . we now need to decide if that data needs to be arranged.')
-	prediction_type = input('but first what type of model would you like to run\
-		 linear or classification: ').lower()
+	print("""Now that your data is loaded and we have determined if it is a time series or not,\
+we now need to decide if that data needs to be arranged.""")
+	prediction_type = int(input("""but first what type of model would you like to run\
+linear or classification. enter 1 for linear and 2 for classification: """))
 	# maybe add an explanation here or linear and classification
-	print('you have chose ' + prediction_type + 'Before we jump into the model. Lets \
-		see if we need to arrange the data. You will have the option to add/change the data,\
-		 remove data, and be able to see certain characteristics of the data. At any point\
-		 type in 4 to jump into the model, but dont worry, before you jump into the \
-		 model you will have the option to save your newly manipulated data into a \
-		 csv file to save right to sql. Lets get started')
+	print("""you have chose ' + prediction_type + 'Before we jump into the model. Lets \
+see if we need to arrange the data. You will have the option to add/change the data,\
+remove data, and be able to see certain characteristics of the data. At any point\
+type in 4 to jump into the model, but dont worry, before you jump into the \
+model you will have the option to save your newly manipulated data into a \
+csv file to save right to sql. Lets get started""")
 	user_interface_input = 0
 	while user_interface_input != 4:
-		if prediction_type == 'linear':
+		if prediction_type == 1:
 			print('this model is not yet supported')
-		elif prediction_type == 'classification':
-			print('Lets get to work on the data, press 1 to bring up the options on add/\
-				change the data, 2 to remove data, 3 to show the data, or 4 to start\
-				the models. (you can save data before model')
+		elif prediction_type == 2:
+			print("""Lets get to work on the data, press 1 to bring up the options on add\
+change the data, 2 to remove data, 3 to show the data, or 4 to start\
+the models. (you can save data before model""")
 			user_interface_input = int(input('please select: '))
 			print(user_interface_input)
 			print(type(user_interface_input))
 			if user_interface_input == 1:
-				print('made it under choice 1')
-				# cant get to next part after one is chosen
-				for x,k in master_menu_actions['add/change'].items():
-					print('select the number for the method you would like and follow the prompt')
-					print(x, 'the method is ' + k[0], 'description: ' + k[1] )
-				print('under for loop choice 1')
+				user_interface_input_add = 0
+				while user_interface_input_add != 9: 
+					# cant get to next part after one is chosen
+					print('enter 9 at any time to go back to main menu')
+					for x,k in master_menu_actions['add/change'].items():
+						print('select the number for the method you would like and follow the prompt')
+						print(x, 'the method is ' + k[0], 'description: ' + k[1] )
+					user_interface_input_add = int(input('please enter method choide here: '))
+					if user_interface_input_add == 1:
+						method_var = list(input('please enter method vars here: ').split(','))
+						print(method_var)
+						print(data_instace.dataframe.head(5))
+						print(type(method_var[0]), method_var[1], method_var[2])
+						data_instace.map_target_for_binary(method_var[0],method_var[1],method_var[2])
+						print(data_instace.dataframe['loan_status'].unique())
+					else:
+						print("""That method is not recongnized. Please select a different \
+							method or press 'q' to exit.""")
+
 			else:
 				print('That is not a menu option')
 
@@ -128,6 +141,8 @@ class UserMenu:
 			break
 	else:
 		print('now going to model section')
+	print(data_instace.dataframe.head(2))
+	print(data_instace.dataframe['loan_status'].unique())
 
 
 	# 2017-01-01 13:50:00,2017-08-07 12:40:00,10Min,date,yes
@@ -139,3 +154,13 @@ class UserMenu:
 
 if __name__ == '__main__':
 	print('ran program')
+
+
+"""
+items = {'item1': 'bill', 'item2': 'cows', 'item3': 'abcdef'}
+choice = input("Select your item: ")
+if choice in items:
+    the_choice = items[choice]
+else:
+    print("Uh oh, I don't know about that item")
+"""
